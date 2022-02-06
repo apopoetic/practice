20b7de8a3f6cb85010249f29a465fab43a153ed3
apopoetic <ankurjamwal@live.com>
2022-02-06 15:37:41 +0530

    Made two different graphs to be combined later

--- lata.R
+++ lata.R
@@ -15,6 +15,36 @@ lata$movie_count <- parse_number(lata$movie_count) #parse movie counts to numeri
 lata$movie_count[58] <- 0
 
 #remove rows with NA
-lata <- lata %>% drop_na()
+lata <- lata %>% drop_na() %>% slice(-71)%>% select(-c("songs_recorded", "movie_count"))
 
+#pivot longer
+lata$year <- as.double(lata$year)
+lata <- lata %>% pivot_longer(cols = c("solo":"other"), names_to = "variables", values_to = "songs") %>%
+  mutate(cum_songs = cumsum(songs))
 
+g1 <- ggplot(data = lata, aes(year, songs, fill = variables)) + geom_area(alpha = 0.75) +
+ theme_classic() + labs(title = "BODY OF LATA MANGESHKAR JI'S WORK", caption = "A. Jamwal\n Source: https://atulsongaday.me/category/series-of-articles/yearwise-review-of-lata-mangeshkar-hf-songs/", size = 10) +
+  xlab("Songs (number)") + ylab("Year") +geom_text(x=2000, y= 200, label = "Cumulative songs = 5255")
+
+#Add awards0
+awards <- data.frame(yr = c(1969, 1989, 1999, 2001, 2008, 1972, 1974, 1990),
+                     award_name = c("Padma Bhushan", 
+                                    "Dada Saheb Phalke Award", 
+                                    "Padma Vibhushan", "Bharat Ratna",
+                                    "Lifetime Achievement Award", 
+                                    "National Award",
+                                    "National Award",
+                                    "National Award"),
+                     n_songs = c(67, 20, 10, 150, 50, 15,40, 80))
+labels <- awards$award_name
+#lollipop graph
+ggplot() + geom_segment(data = awards, aes(x=yr, xend = yr, y=0, yend=n_songs)) +
+  geom_point(data = awards, aes(x =yr, y=n_songs), size =3, colour = "red", 
+             fill = alpha("orange", 0.5), alpha = 0.75, shape = 21) + 
+  geom_text(data = awards, aes(x =yr, y=n_songs), label= labels, vjust  = -1.5) + 
+  xlim(1955, 2022)
+
+#combine two graphs
+g1 <- ggplot(data = lata, aes(year, songs, fill = variables)) + geom_area(alpha = 0.75) +
+  theme_classic() + labs(title = "BODY OF LATA MANGESHKAR JI'S WORK", caption = "A. Jamwal\n Source: https://atulsongaday.me/category/series-of-articles/yearwise-review-of-lata-mangeshkar-hf-songs/", size = 10) +
+  xlab("Songs (number)") + ylab("Year") +geom_text(x=2000, y= 200, label = "Cumulative songs = 5255")
